e676fa3c3644c574916ad539a699d880c4f2fb6e
stetimi <123815784+stetimi@users.noreply.github.com>
2026-04-03 16:26:08 +0800

    add ROM binary data sets

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use winit::event_loop::EventLoop;
 fn main() -> Result<(), EventLoopError> {
     let event_loop = EventLoop::new()?;
     let mut screen = Screen::new();
-    let memory = Memory::default();
+    let mut memory = Memory::default();
+    memory.load_standard_roms_from_data_dir();
     let mut vic = VIC::new(&memory);
     vic.set_border_color(4); // purple border
     let framebuffer = vic.render_frame();
--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 pub struct Memory {
     pub bytes: [u8; 65536],
 }
@@ -5,5 +7,26 @@ pub struct Memory {
 impl Default for Memory {
     fn default() -> Self {
         Self { bytes: [0; 65536] }
+    }
+}
+
+const CHARACTER_ROM_START: usize = 0x8000;
+const BASIC_ROM_START: usize = 0xC000;
+const KERNEL_ROM_START: usize = 0xE000;
+
+impl Memory {
+    fn load_rom(&mut self, data: &[u8], start_address: usize) {
+        let end_address = start_address + data.len();
+        self.bytes[start_address..end_address].copy_from_slice(data);
+    }
+
+    pub fn load_standard_roms_from_data_dir(&mut self) {
+        let basic_rom = fs::read("data/basic.901486-01.bin").expect("Missing basic_rom");
+        let characters_rom = fs::read("data/characters.901460-03.bin").expect("Missing characters_rom");
+        let kernal_rom = fs::read("data/kernal.901486-07.bin").expect("Missing kernal_rom");
+
+        self.load_rom(&basic_rom, BASIC_ROM_START);
+        self.load_rom(&characters_rom, CHARACTER_ROM_START);
+        self.load_rom(&kernal_rom, KERNEL_ROM_START);
     }
 }
